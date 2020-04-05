477a9ccb482af84eaf34f70be19dbd7e01021bb7
Wei-Yu Chen <aweimeow.tw@gmail.com>
2020-04-05 14:58:40 +0800

    Update layout with customized style
    homepage: 3 columns
    posts and pages: 2 columns

--- layout/layout.jsx
+++ layout/layout.jsx
@@ -10,10 +10,8 @@ const Search = require('./common/search');
 module.exports = class extends Component {
     render() {
         const { env, site, config, page, helper, body } = this.props;
-
         const language = page.lang || page.language || config.language;
-        const columnCount = Widgets.getColumnCount(config.widgets);
-
+        const columnCount = (page.__post == true || page.__page == true) && 2 || Widgets.getColumnCount(config.widgets);
         return <html lang={language ? language.substr(0, 2) : ''}>
             <Head env={env} site={site} config={config} helper={helper} page={page} />
             <body class={`is-${columnCount}-column`}>
@@ -26,11 +24,13 @@ module.exports = class extends Component {
                                 'order-2': true,
                                 'column-main': true,
                                 'is-12': columnCount === 1,
-                                'is-8-tablet is-8-desktop is-8-widescreen': columnCount === 2,
+                                'is-9-tablet is-9-desktop is-9-widescreen': columnCount === 2,
                                 'is-8-tablet is-8-desktop is-6-widescreen': columnCount === 3
                             })} dangerouslySetInnerHTML={{ __html: body }}></div>
                             <Widgets site={site} config={config} helper={helper} page={page} position={'left'} />
-                            <Widgets site={site} config={config} helper={helper} page={page} position={'right'} />
+                            {(page.__post != true && page.__page != true) &&
+                                <Widgets site={site} config={config} helper={helper} page={page} position={'right'} />
+                            }
                         </div>
                     </div>
                 </section>
